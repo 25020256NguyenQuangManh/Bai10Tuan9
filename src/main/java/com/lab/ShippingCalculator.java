d2cc5b387a213ad0eacad112d269b7f04d624ac3
25020256NguyenQuangManh <25020256@vnu.edu.vn>
2026-05-04 20:19:40 +0700

    Create intentional build failure

--- src/main/java/com/lab/ShippingCalculator.java
+++ src/main/java/com/lab/ShippingCalculator.java
@@ -7,7 +7,7 @@ public class ShippingCalculator {
             throw new IllegalArgumentException("Weight must be positive");
         }
         if (type.equals("EXPRESS")) return weight * 5000 + 20000;
-        if (type.equals("STANDARD")) return weight * 3000;
+        if (type.equals("STANDARD")) return weight * 3000 + 1;
         throw new IllegalArgumentException("Unknown type: " + type);
     }
 }
